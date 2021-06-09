300d4a12a24324282bcb46b5e28ade0e64b31656
Der-Henning <henning.merklinger@gmail.com>
2021-06-09 08:24:36 +0000

    created migration script from old mopsy

---
+++ server/src/scripts/migrate_mopsy_php.js
@@ -0,0 +1,32 @@
+const models = require("../models");
+const mysql = require('mysql');
+
+const migrate = () => {
+    const old_db = mysql.createConnection({
+        host: process.env.MIGRATE_OLD_MYSQL_HOST,
+        user: process.env.MIGRATE_OLD_MYSQL_USERNAME,
+        password: process.env.MIGRATE_OLD_MYSQL_PASSWORD,
+        database: process.env.MIGRATE_OLD_MYSQL_DATABASE
+    });
+
+    models.sequelize.sync().then(() => {
+        old_db.connect().then(async () => {
+
+            const users_old = await old_db.query("SELECT * FROM login");
+            for (user in users_old) {
+                var hash = "$2b$10$" + user.password.slice(7);
+                var login = await models.Login.create({
+                    username: user.username,
+                    email: user.email,
+                    password: hash
+                });
+                console.log(`created ${login}`)
+            }
+
+        })
+            .catch(err => { throw err })
+    })
+        .catch(err => { throw err })
+}
+
+export default migrate
